c07c8519a508e73327c94dca263edea615e8ae74
Jeff Becker <jeff@i2p.rocks>
2018-04-17 08:34:37 -0400

    fug

--- lib/pleroma/http/http.ex
+++ lib/pleroma/http/http.ex
@@ -5,7 +5,7 @@ defmodule Pleroma.HTTP do
 
   defp parse_proxy_url(url) do
     uri = URI.parse(url)
-    case uri.protocol do
+    case uri.scheme do
       "socks" -> {:socks5 , uri.host, uri.port}
       _ -> {:connect , uri.host, uri.port}
     end
