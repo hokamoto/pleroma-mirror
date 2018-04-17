26177dbc77b2755556505a36f2901a985f6c43c4
Jeff Becker <jeff@i2p.rocks>
2018-04-17 08:03:31 -0400

    i suck at elixir

--- lib/pleroma/http/http.ex
+++ lib/pleroma/http/http.ex
@@ -10,4 +10,36 @@ defmodule Pleroma.HTTP do
       _ -> options ++ [proxy: proxy]
     end
   end
+
+  defp filter_opts_using_proxy_map(url, options, proxy_map) do
+    uri = URI.parse(url)
+    item = Enum.find(proxy_map, nil, fn (idx) -> Regex.match?(Enum.at(idx, 0), uri.host) end)
+    case item do
+      nil -> options
+      _ ->
+        case Enum.at(item, 1) do
+          nil -> Keyword.drop(options, [:proxy])
+          "" -> Keyword.drop(options, [:proxy])
+          _ -> Keyword.put(options, :proxy, Enum.at(item, 1))
+        end
+    end
+  end
+
+  defp process_opts(url, options) do
+    config = Application.get_env(:pleroma, :http, [])
+    proxy_map = Keyword.get(config, :proxy_map, nil)
+    case proxy_map do
+      nil -> options
+      _ -> filter_opts_using_proxy_map(url, options, proxy_map)
+    end
+  end
+  
+  def request(method, url, body \\ "", headers \\ [], options \\ []) do
+    HTTPoison.request(method, url, body, headers, process_opts(url, options))
+  end
+
+  def request!(method, url, body \\ "", headers \\ [], options \\ []) do
+    HTTPoison.request!(method, url, body, headers, process_opts(url, options))
+  end
+
 end
