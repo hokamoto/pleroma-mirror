f484c5fec71c0e0b6c55d4b9dc1bbb760409c7f7
Tristan Mahé <gled@remote-shell.net>
2018-06-20 16:09:52 -0700

    autoCW negative posts and add a summary

--- lib/pleroma/web/activity_pub/mrf/sentiment_analysis.ex
+++ lib/pleroma/web/activity_pub/mrf/sentiment_analysis.ex
@@ -6,36 +6,38 @@ defmodule Pleroma.Web.ActivityPub.MRF.SentimentAnalysis do
 
   @rate_post Keyword.get(@mrf_policy, :sentiment_analysis_posts)
   @do_auto_cw Keyword.get(@mrf_policy, :sentiment_analysis_autocw)
-  defp do_rate_post(actor_info, object) do
+  defp do_rate_post(object) do
     child_object = object["object"]
+    grade = 0
 
-    if @rate_post do
+    if @rate_post == true do
       if child_object["content"] != nil do
         grade = Veritaserum.analyze(child_object["content"])
-        Logger.info("rating found #{inspect(child_object)}:#{grade}")
-      else
-        grade = 0
+        Logger.info("Found content:#{grade}")
       end
 
-      if @do_auto_cw and grade < -2 do
+      if @do_auto_cw == true and grade < -1 do
+        Logger.info("Marked sensitive !")
         child_object = Map.put(child_object, "sensitive", true)
+
+        if child_object["summary"] != nil do
+          child_object =
+            Map.put(child_object, "summary", "[Negative: #{grade}]" <> child_object["summary"])
+        else
+          child_object = Map.put(child_object, "summary", "[Negative: #{grade}]")
+        end
       end
 
       child_object = Map.put(child_object, "sentiment_analysis", grade)
       object = Map.put(object, "object", child_object)
     end
 
+    Logger.info("Sentiment_analysis: #{inspect(child_object)}")
     {:ok, object}
   end
 
   @impl true
   def filter(object) do
-    actor_info = URI.parse(object["actor"])
-
-    with {:ok, object} <- do_rate_post(actor_info, object) do
-      {:ok, object}
-    else
-      _e -> {:reject, nil}
-    end
+    do_rate_post(object)
   end
 end
