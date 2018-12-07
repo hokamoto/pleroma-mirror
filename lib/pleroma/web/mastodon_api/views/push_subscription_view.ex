383926e47556ea71767637c38228a50d9c2d6c8d
Egor Kislitsyn <egor@kislitsyn.com>
2018-12-07 15:55:28 +0700

    fix and improve web push; add configuration docs

--- lib/pleroma/web/mastodon_api/views/push_subscription_view.ex
+++ lib/pleroma/web/mastodon_api/views/push_subscription_view.ex
@@ -1,12 +1,16 @@
 defmodule Pleroma.Web.MastodonAPI.PushSubscriptionView do
   use Pleroma.Web, :view
-  alias Pleroma.Web.MastodonAPI.PushSubscriptionView
 
   def render("push_subscription.json", %{subscription: subscription}) do
     %{
       id: to_string(subscription.id),
       endpoint: subscription.endpoint,
-      alerts: Map.get(subscription.data, "alerts")
+      alerts: Map.get(subscription.data, "alerts"),
+      server_key: server_key()
     }
+  end
+
+  defp server_key do
+    Keyword.get(Application.get_env(:web_push_encryption, :vapid_details), :public_key)
   end
 end
