c2af9904080fffd67bdcf576c2ac88f7eff9ba15
Ivan Tashkinov <ivantashkinov@gmail.com>
2018-12-22 12:53:26 +0300

    Fixes a floating, CI-specific unit test failure: `test/plugs/session_authentication_plug_test.exs:18: Pleroma.User.__struct__/1 is undefined, cannot expand struct Pleroma.User`

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,3 +2,6 @@ ExUnit.start()
 
 Ecto.Adapters.SQL.Sandbox.mode(Pleroma.Repo, :manual)
 {:ok, _} = Application.ensure_all_started(:ex_machina)
+
+# Fixes CI unit test error: ** (CompileError) Pleroma.User.__struct__/1 is undefined, cannot expand struct Pleroma.User
+true = Code.ensure_compiled?(Pleroma.User)
