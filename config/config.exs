cd352f1b4c47408916b01c4dde4b60fcf963a58c
Maksim Pechnikov <parallel588@gmail.com>
2019-04-22 17:04:59 +0300

    update default oauth2 config

--- config/config.exs
+++ config/config.exs
@@ -467,7 +467,7 @@ config :pleroma, Pleroma.ScheduledActivity,
 
 config :pleroma, :oauth2,
   token_expires_in: 600,
-  issue_new_refresh_token: false
+  issue_new_refresh_token: true
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
