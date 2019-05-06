fd83c615977add0e5384ce018de8883fd9406fc6
xse <xse@riseup.net>
2019-05-06 23:26:05 +0200

    ex_doc: source_url is outside the docs key

--- mix.exs
+++ mix.exs
@@ -17,8 +17,8 @@ defmodule Pleroma.Mixfile do
       # Docs
       name: "Pleroma",
       homepage_url: "https://pleroma.social/",
+      source_url: "https://git.pleroma.social/pleroma/pleroma",
       docs: [
-        source_url: "https://git.pleroma.social/pleroma/pleroma",
         source_url_pattern: "https://git.pleroma.social/pleroma/pleroma/blob/develop/%{path}#L%{line}",
         logo: "priv/static/static/logo.png",
         extras: ["README.md", "CHANGELOG.md"] ++ Path.wildcard("docs/**/*.md"),
