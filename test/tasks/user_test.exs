18bdf03ae0bddc874873372148da8cbc66e54ab5
Sergey Suprunenko <suprunenko.s@gmail.com>
2019-05-16 11:45:58 +0200

    Remove duplicated entries in users' following lists

--- test/tasks/user_test.exs
+++ test/tasks/user_test.exs
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 
 defmodule Mix.Tasks.Pleroma.UserTest do
+  alias Pleroma.Repo
   alias Pleroma.User
   use Pleroma.DataCase
 
@@ -363,6 +364,34 @@ defmodule Mix.Tasks.Pleroma.UserTest do
       user = Repo.get(User, id)
       refute user.info.confirmation_pending
       refute user.info.confirmation_token
+    end
+  end
+
+  describe "running update_following_followers_counts" do
+    test "following and followers count are updated" do
+      [user, user2] = insert_pair(:user)
+      {:ok, %User{following: following, info: info} = user} = User.follow(user, user2)
+
+      assert length(following) == 2
+      assert info.follower_count == 0
+
+      info_cng = Ecto.Changeset.change(info, %{follower_count: 3})
+
+      {:ok, user} =
+        user
+        |> Ecto.Changeset.change(%{following: following ++ following})
+        |> Ecto.Changeset.put_embed(:info, info_cng)
+        |> Repo.update()
+
+      assert length(user.following) == 4
+      assert user.info.follower_count == 3
+
+      assert :ok == Mix.Tasks.Pleroma.User.run(["update_following_followers_counts"])
+
+      user = User.get_by_id(user.id)
+
+      assert length(user.following) == 2
+      assert user.info.follower_count == 0
     end
   end
 end
