703ea1b3fbfb2ffd75472387efbb38978ac4456b
Jeff Becker <jeff@i2p.rocks>
2019-05-27 06:58:01 -0400

    simplify call for config

--- lib/pleroma/http/connection.ex
+++ lib/pleroma/http/connection.ex
@@ -32,8 +32,7 @@ defmodule Pleroma.HTTP.Connection do
   defp hackney_options(opts) do
     options = Keyword.get(opts, :adapter, [])
     adapter_options = Pleroma.Config.get([:http, :adapter], [])
-    config = Application.get_env(:pleroma, :http, [])
-    proxy_url = Keyword.get(config, :proxy_url, nil)
+    proxy_url = Pleroma.Config.get([:http, :proxy_url], nil)
 
     @hackney_options
     |> Keyword.merge(adapter_options)
