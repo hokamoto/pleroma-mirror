856856c17c632005d42cb2b99ae5f71dc4c923a5
Alex S <alex.strizhakov@gmail.com>
2019-06-19 13:12:55 +0300

    migrations_path to rollback task

--- lib/mix/tasks/pleroma/ecto/migrate.ex
+++ lib/mix/tasks/pleroma/ecto/migrate.ex
@@ -5,6 +5,7 @@
 defmodule Mix.Tasks.Pleroma.Ecto.Migrate do
   use Mix.Task
   require Logger
+
   @shortdoc "Wrapper on `ecto.migrate` task."
 
   @aliases [
@@ -48,7 +49,7 @@ defmodule Mix.Tasks.Pleroma.Ecto.Migrate do
         do: Keyword.merge(opts, log: false, log_sql: false),
         else: opts
 
-    path = ensure_migrations_path(Pleroma.Repo, opts)
+    path = Mix.Tasks.Pleroma.Ecto.ensure_migrations_path(Pleroma.Repo, opts)
 
     level = Logger.level()
     Logger.configure(level: :info)
@@ -56,41 +57,5 @@ defmodule Mix.Tasks.Pleroma.Ecto.Migrate do
     {:ok, _, _} = Ecto.Migrator.with_repo(Pleroma.Repo, &Ecto.Migrator.run(&1, path, :up, opts))
 
     Logger.configure(level: level)
-  end
-
-  @doc """
-  Ensures the given repository's migrations path exists on the file system.
-  """
-  @spec ensure_migrations_path(Ecto.Repo.t(), Keyword.t()) :: String.t()
-  def ensure_migrations_path(repo, opts) do
-    path = opts[:migrations_path] || Path.join(source_repo_priv(repo), "migrations")
-
-    if not File.dir?(path) do
-      raise_missing_migrations(Path.relative_to_cwd(path), repo)
-    end
-
-    path
-  end
-
-  @doc """
-  Returns the private repository path relative to the source.
-  """
-  def source_repo_priv(repo) do
-    config = repo.config()
-    priv = config[:priv] || "priv/#{repo |> Module.split() |> List.last() |> Macro.underscore()}"
-    Path.join(File.cwd!(), priv)
-  end
-
-  defp raise_missing_migrations(path, repo) do
-    raise("""
-    Could not find migrations directory #{inspect(path)}
-    for repo #{inspect(repo)}.
-    This may be because you are in a new project and the
-    migration directory has not been created yet. Creating an
-    empty directory at the path above will fix this error.
-    If you expected existing migrations to be found, please
-    make sure your repository has been properly configured
-    and the configured path exists.
-    """)
   end
 end
--- lib/mix/tasks/pleroma/ecto/rollback.ex
+++ lib/mix/tasks/pleroma/ecto/rollback.ex
@@ -48,13 +48,16 @@ defmodule Mix.Tasks.Pleroma.Ecto.Rollback do
         do: Keyword.merge(opts, log: false, log_sql: false),
         else: opts
 
+    path = Mix.Tasks.Pleroma.Ecto.ensure_migrations_path(Pleroma.Repo, opts)
+
     level = Logger.level()
     Logger.configure(level: :info)
 
     if Pleroma.Config.get(:env) == :test do
       Logger.info("Rollback succesfully")
     else
-      {:ok, _, _} = Ecto.Migrator.with_repo(Pleroma.Repo, &Ecto.Migrator.run(&1, :down, opts))
+      {:ok, _, _} =
+        Ecto.Migrator.with_repo(Pleroma.Repo, &Ecto.Migrator.run(&1, path, :down, opts))
     end
 
     Logger.configure(level: level)
