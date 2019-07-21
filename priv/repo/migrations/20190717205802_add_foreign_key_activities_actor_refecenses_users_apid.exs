7905e151622a2dd913df8205efe58fce93af1cb5
kPherox <admin@mail.kr-kp.com>
2019-07-21 15:37:57 +0900

    Fix rollback error

--- priv/repo/migrations/20190717205802_add_foreign_key_activities_actor_refecenses_users_apid.exs
+++ priv/repo/migrations/20190717205802_add_foreign_key_activities_actor_refecenses_users_apid.exs
@@ -1,11 +1,15 @@
 defmodule Pleroma.Repo.Migrations.AddForeignKeyActivitiesActorReferencesUsersApid do
   use Ecto.Migration
 
-  def change do
+  def up do
     execute "DELETE FROM activities WHERE activities.actor not in (SELECT ap_id FROM users)"
 
     alter table :activities do
       modify :actor, references(:users, type: :varchar, column: :ap_id, on_delete: :delete_all, on_update: :update_all)
     end
+  end
+
+  def down do
+    drop constraint(:activities, "activities_actor_fkey")
   end
 end
