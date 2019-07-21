d254723bd8a98ef3264639171e43df85a2a01081
Sadposter <hannah+pleroma@coffee-and-dreams.uk>
2019-07-21 16:01:20 +0100

    Add test for blocked user notification streams

--- test/web/streamer_test.exs
+++ test/web/streamer_test.exs
@@ -65,6 +65,20 @@ defmodule Pleroma.Web.StreamerTest do
       Streamer.stream("user:notification", notify)
       Task.await(task)
     end
+
+    test "it doesn't send notify to the 'user' stream when a user is blocked", %{user: user} do
+        task =
+            Task.async(fn -> refute_receive {:text, _}, 4_000 end)
+        Streamer.add_socket("user", %{transport_pid: task.pid, assigns: %{user: user}})
+
+        blocked = insert(:user)
+        {:ok, user} = User.block(user, blocked)
+        {:ok, activity} = CommonAPI.post(user, %{"status" => ":("})
+        {:ok, notif, _} = CommonAPI.favorite(activity.id, blocked)
+        
+        Streamer.stream("user", notif)
+        Task.await(task)
+    end
   end
 
   test "it sends to public" do
