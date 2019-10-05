54d6002d98f7879fccf0f3cd498a14a6db1e17e8
Ariadne Conill <ariadne@dereferenced.org>
2019-10-05 20:06:03 +0000

    user info: add invisible field

--- lib/pleroma/user/info.ex
+++ lib/pleroma/user/info.ex
@@ -56,6 +56,7 @@ defmodule Pleroma.User.Info do
     field(:fields, {:array, :map}, default: nil)
     field(:raw_fields, {:array, :map}, default: [])
     field(:discoverable, :boolean, default: false)
+    field(:invisible, :boolean, default: false)
 
     field(:notification_settings, :map,
       default: %{
@@ -397,6 +398,14 @@ defmodule Pleroma.User.Info do
     info
     |> cast(params, [:source_data])
     |> validate_required([:source_data])
+  end
+
+  def set_invisible(info, invisible) do
+    params = %{invisible: invisible}
+
+    info
+    |> cast(params, [:invisible])
+    |> validate_required([:invisible])
   end
 
   def admin_api_update(info, params) do
