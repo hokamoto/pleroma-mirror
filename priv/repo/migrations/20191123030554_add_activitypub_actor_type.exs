c6b9cf29e0a2c5ad8d2dc8b5626786c8b79cc348
Hakaba Hitoyo <hakabahitoyo@yahoo.co.jp>
2019-11-23 14:19:53 +0900

    formatting

--- priv/repo/migrations/20191123030554_add_activitypub_actor_type.exs
+++ priv/repo/migrations/20191123030554_add_activitypub_actor_type.exs
@@ -3,7 +3,7 @@ defmodule Pleroma.Repo.Migrations.AddActivitypubActorType do
 
   def change do
     alter table("users") do
-      add :actor_type, :string, default: "Person"
+      add(:actor_type, :string, default: "Person")
     end
   end
 end
