f75926292a5dbdbf8fbd17c5c99d5c287a7b57ec
Alexander <alex.strizhakov@gmail.com>
2019-12-09 18:54:44 +0300

    removing index

--- priv/repo/migrations/20191209082339_add_attachment_index_to_objects.exs
+++
@@ -1,9 +0,0 @@
-defmodule Pleroma.Repo.Migrations.AddAttachmentIndexToObjects do
-  use Ecto.Migration
-
-  def change do
-    create_if_not_exists(
-      index(:objects, ["(data->'attachment')"], using: :gin, name: :objects_attachments)
-    )
-  end
-end
